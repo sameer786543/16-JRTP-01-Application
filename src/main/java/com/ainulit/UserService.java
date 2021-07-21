8ec26e02b7ee5745ba8dc5cff5b299132f321289
Ainul <aliainul9568@gmail.com>
2021-07-21 16:46:00 +0530

    deleted

--- src/main/java/com/ainulit/UserService.java
+++ src/main/java/com/ainulit/UserService.java
@@ -1,7 +1,7 @@
 package com.ainulit;
 
 public class UserService {
-        private  static final  String URL="http://www.ainulit.com";
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

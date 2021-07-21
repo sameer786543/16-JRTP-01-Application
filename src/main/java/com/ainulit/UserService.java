236183cf002d8a2accce90a04dc3f52e710e43e8
Ainul <aliainul9568@gmail.com>
2021-07-21 15:15:44 +0530

    add variables

--- src/main/java/com/ainulit/UserService.java
+++ src/main/java/com/ainulit/UserService.java
@@ -5,6 +5,9 @@ public class UserService {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to visit our Website...");
+		int a=10;
+		int b=20;
+		
 	}
 
 }

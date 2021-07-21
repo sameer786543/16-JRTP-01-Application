f377bee5516e38435f45d5f7e69f062f62f13f8a
Ainul <aliainul9568@gmail.com>
2021-07-21 15:04:40 +0530

    first commit

---
+++ src/main/java/com/ainulit/UserService.java
@@ -0,0 +1,10 @@
+package com.ainulit;
+
+public class UserService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to visit our Website...");
+	}
+
+}

49ebdd1f7f485d76c2dfcf288cdf8a3588318f02
Ainul <aliainul9568@gmail.com>
2021-07-21 16:58:19 +0530

    adding url

--- src/main/java/com/ainulit/UserService.java
+++ src/main/java/com/ainulit/UserService.java
@@ -1,7 +1,7 @@
 package com.ainulit;
 
 public class UserService {
-
+         private static final String URL="www.ainulit.com";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to visit our Website...");
